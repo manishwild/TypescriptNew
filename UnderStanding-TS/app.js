42e81c47ad16e6b917e7cab416387e244fd28256
manishwild <manishwild1000@gmail.com>
2021-07-26 08:14:20 +0200

    lastweek

--- UnderStanding-TS/app.js
+++ UnderStanding-TS/app.js
@@ -1,4 +1,13 @@
-// the Unknown type
+// the Unknown type. and also it is better than any.we should not use all the time.
 var userInput;
+var userName;
 userInput = 5;
 userInput = 'Preetish';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+// never type
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An Error Occured!', 500);
--- UnderStanding-TS/app.ts
+++ UnderStanding-TS/app.ts
@@ -1,4 +1,4 @@
-// the Unknown type
+// the Unknown type. and also it is better than any.we should not use all the time.
 let userInput: unknown
 let userName: string
 
@@ -7,3 +7,10 @@ userInput = 'Preetish'
 if (typeof userInput === 'string') {
   userName = userInput
 }
+
+// never type. it will never return anything
+function generateError (message: string, code: number): never {
+  throw { message: message, errorCode: code }
+  // while (true) {}
+}
+generateError('An Error Occured!', 500)
