937e52e8ba9bf7e3b65236178a47969e90a1c05f
manishwild <manishwild1000@gmail.com>
2021-08-13 08:59:15 +0200

    react-typsript

--- lodash/src/app.ts
+++ lodash/src/app.ts
@@ -1,3 +1,39 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
-console.log(_.shuffle([1, 2, 3]));
+// console.log(_.shuffle([1, 2, 3]));
+// declare var Global: string
+// console.log(Global)
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+
+import { Product } from './product.model';
+
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 10.99 }
+];
+
+const newProd = new Product('', -5.99);
+validate(newProd).then(errors => {
+  if (errors.length > 0) {
+    console.log('VALIDATION ERRORS!');
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
+
+// const p1 = new Product('A Book', 12.99);
+
+// const loadedProducts = products.map(prod => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
+
+
